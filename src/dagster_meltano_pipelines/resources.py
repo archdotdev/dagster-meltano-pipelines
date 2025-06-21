4991d764abdb967a7dcf98e86c404d39579b5d7c
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-06-21 13:56:36 -0600

    Support more extractor and loader extras (#11)

--- src/dagster_meltano_pipelines/resources.py
+++ src/dagster_meltano_pipelines/resources.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Any, Dict, List, Optional
 
 import dagster as dg
@@ -15,10 +16,48 @@ class MeltanoPluginConfig(dg.PermissiveConfig):
 class ExtractorConfig(MeltanoPluginConfig):
     """Extractor configuration."""
 
-    select: Optional[List[str]] = Field(description="Stream selection", alias="_select")
+    # https://github.com/meltano/meltano/blob/0dad7d51a36862e3df7a9f5cf19425540577e5c9/src/meltano/core/plugin/singer/tap.py#L172
+    catalog: Optional[Path] = Field(
+        description="Stream catalog file",
+        alias="_catalog",
+    )
+    state: Optional[Path] = Field(
+        description="Stream state file",
+        alias="_state",
+    )
+    load_schema: Optional[str] = Field(
+        description="Load data to this schema in the target database",
+        alias="_load_schema",
+    )
+    select: Optional[List[str]] = Field(
+        description="Stream selection",
+        alias="_select",
+    )
     metadata: Optional[Dict[str, Any]] = Field(
         description="Stream metadata",
         alias="_metadata",
+    )
+    tap_schema: Optional[Dict[str, Any]] = Field(
+        description="Stream schema",
+        alias="_schema",
+    )
+    select_filter: Optional[List[str]] = Field(
+        description="Stream selection filter",
+        alias="_select_filter",
+    )
+    use_cached_catalog: bool = Field(
+        description="Use cached catalog",
+        alias="_use_cached_catalog",
+    )
+
+
+class LoaderConfig(MeltanoPluginConfig):
+    """Loader configuration."""
+
+    # https://github.com/meltano/meltano/blob/0dad7d51a36862e3df7a9f5cf19425540577e5c9/src/meltano/core/plugin/singer/target.py#L105
+    dialect: Optional[str] = Field(
+        description="Target database dialect",
+        alias="_dialect",
     )
 
 
@@ -26,17 +65,13 @@ class MeltanoPlugin(dg.ConfigurableResource["MeltanoPlugin"]):
     """Base class for Meltano plugins."""
 
     name: str = Field(description="The Meltano plugin name")
-    config: Optional[MeltanoPluginConfig] = Field(
-        description="The Meltano plugin configuration",
-    )
+    config: Optional[MeltanoPluginConfig] = Field(description="The Meltano plugin configuration")
 
 
 class Extractor(MeltanoPlugin):
     """Extractor."""
 
-    config: Optional[ExtractorConfig] = Field(
-        description="The Meltano extractor configuration",
-    )
+    config: Optional[ExtractorConfig] = Field(description="The Meltano extractor configuration")
 
 
 class Loader(MeltanoPlugin):
