cee990bc7c70c198930bcf191b148a17d0ed1079
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-07-23 12:54:35 -0600

    feat: Support more general asset props

--- src/dagster_meltano_pipelines/components/meltano_pipeline/__init__.py
+++ src/dagster_meltano_pipelines/components/meltano_pipeline/__init__.py
@@ -1,9 +1,11 @@
 from .component import (
+    DagsterAssetProps,
     MeltanoPipeline,
     MeltanoPipelineComponent,
 )
 
 __all__ = [
+    "DagsterAssetProps",
     "MeltanoPipelineComponent",
     "MeltanoPipeline",
 ]
